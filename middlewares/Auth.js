c9f31d2ebdb6e537b1de0bb15a4c3c37deefced5
Sarthak Sharma <ssarthak116@protonmail.ch>
2023-04-16 00:06:43 +0530

    auth middleware

--- middlewares/Auth.js
+++ middlewares/Auth.js
@@ -1,7 +1,29 @@
-function Auth(req, res, next) {
-    console.log(req);
-    next(); // Call the next middleware function in the chain
+const jwt = require('jsonwebtoken');
+
+function auth(request, response, next) {
+  const authHeader = req.headers.authorization;
+  const token = authHeader && authHeader.split(' ')[1];
+
+  if (!token) {
+
+    res.status(401).json({ error: 'Unauthorized: Missing token' });
+
   }
-  
-  module.exports = Auth;
-  
+
+  try {
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+
+  } catch (err) {
+
+    return res.status(401).json({ error: 'Unauthorized: Invalid or expired token' });
+
+  }
+}
+
+module.exports = auth;
+
+// Authorization: Bearer mytoken123
+
